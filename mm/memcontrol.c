8bfc8e649da19bb4bb74138b8b82ea67af56069a
Darren Smith <nerevar@gmail.com>
2014-10-13 23:19:09 -0500

    3.4.83
    Change-Id: Ie96a6d24535954d3c5abbb217303e3fd40d7f741

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -1144,25 +1144,15 @@ struct lruvec *mem_cgroup_lru_move_lists(struct zone *zone,
  * Checks whether given mem is same or in the root_mem_cgroup's
  * hierarchy subtree
  */
-bool __mem_cgroup_same_or_subtree(const struct mem_cgroup *root_memcg,
-				  struct mem_cgroup *memcg)
-{
-	if (root_memcg == memcg)
-		return true;
-	if (!root_memcg->use_hierarchy)
-		return false;
-	return css_is_ancestor(&memcg->css, &root_memcg->css);
-}
-
 static bool mem_cgroup_same_or_subtree(const struct mem_cgroup *root_memcg,
-				       struct mem_cgroup *memcg)
+				struct mem_cgroup *memcg)
 {
-	bool ret;
+	if (root_memcg != memcg) {
+		return (root_memcg->use_hierarchy &&
+			css_is_ancestor(&memcg->css, &root_memcg->css));
+	}
 
-	rcu_read_lock();
-	ret = __mem_cgroup_same_or_subtree(root_memcg, memcg);
-	rcu_read_unlock();
-	return ret;
+	return true;
 }
 
 int task_in_mem_cgroup(struct task_struct *task, const struct mem_cgroup *memcg)
